447d585fb782fa2f4f4b0649d5954d9c1dea0d4c
ako80218 <andrew@andrewotto.com>
2014-01-23 15:32:14 -0700

    two fields are connected to the form

--- main.js
+++ main.js
@@ -1,30 +1,47 @@
 $(document).ready(function(){
-	var profileButton = $('#edit-profile-button');
-	var formSubmit = $('#form-submit');
+	var $profileButton = $('#edit-profile-button');
+	var $formSubmit = $('#form-submit');
 	//get handle on #user-form
-	var form = $('#user-form');
+	var $form = $('#user-form');
+	var $nameInput = $('#name-input');
+	var $nameField = $('#name-field');
+	var $bioTextArea =$('#bio-text-area');
+	var $bioField = $('#bio-field');
 	//get handle on #profile-content
-	var profileContent = $('#profile-content');
+	var $profileContent = $('#profile-content');
 	var toggleForm = function(element){
 		element.toggle();
 		
 	};
 
-	var displayElement = function(element){
-		element.removeClass('hidden');
-	};
-	var hideElement = function(element){
-		element.addClass('hidden');
+	var updateTextElement = function(element){
+
 	};
 
-	profileButton.click(function(e){
-		toggleForm(form);
+	// var displayElement = function(element){
+	// 	element.removeClass('hidden');
+	// };
+	// var hideElement = function(element){
+	// 	element.addClass('hidden');
+	// };
+
+	$profileButton.click(function(e){
+		toggleForm($form);
 		e.preventDefault();
 	});
 
-	formSubmit.click(function(e){
-		toggleForm(form);
+	$formSubmit.click(function(e){
+		toggleForm($form);
+		var nameValue = $nameInput.val();
+		console.log(nameValue);
+		$nameField.text(nameValue);
+		var bioValue = $bioTextArea.val();
+		$bioField.text(bioValue);
+
 		e.preventDefault();
+	
+
+
 	 });
 
 
