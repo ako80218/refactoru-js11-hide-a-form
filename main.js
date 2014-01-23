8bc13a55f429f8fb4d91f9dd629385a377b338a9
ako80218 <andrew@andrewotto.com>
2014-01-23 06:33:02 -0700

    hides two main elements

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 $(document).ready(function(){
 	var profileButton = $('#edit-profile-button');
+	var formSubmit = $('#form-submit');
+
+
+	//get handle on #user-form
+	var form = $('#user-form');
+	//get handle on #profile-content
+	var profileContent = $('#profile-content');
+
 
 	profileButton.click(function(){
 
 		// console.log('clicked');
-		var form =
+		// remove class hiding the form
+		form.removeClass('hidden');
+		profileContent.addClass('hidden');
+	});
+	formSubmit.click(function(){
+
+		// console.log('clicked');
+		// remove class hiding the form
+		form.addClass('hidden');
+		profileContent.removeClass('hidden');
 	});
 
 });
